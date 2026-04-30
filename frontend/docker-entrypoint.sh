ea8e295d12ecd4375b88f2d22084e141bacf82d2
20230301411_EYGS <giacomo.rossi4@it.ey.com>
2026-04-30 17:48:16 +0200

    Set default port in entrypoint script and update nginx template to use it

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 # Substitute only ${API_URL} in the nginx template, leave all other nginx
 # variables ($host, $http_upgrade, $uri, etc.) untouched.
+# Set default port if not provided by Railway
+export PORT=${PORT:-80}
+
 envsubst '${API_URL} ${PORT}' < /etc/nginx/templates/default.conf.template \
     > /etc/nginx/conf.d/default.conf
 
