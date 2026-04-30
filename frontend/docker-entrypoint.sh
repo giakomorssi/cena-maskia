35aa0a5df2e508a93316444b7a96ffb22830e4d1
20230301411_EYGS <giacomo.rossi4@it.ey.com>
2026-04-30 17:38:50 +0200

    Update nginx configuration and railway.toml for improved environment variable handling

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Substitute only ${API_URL} in the nginx template, leave all other nginx
 # variables ($host, $http_upgrade, $uri, etc.) untouched.
-envsubst '${API_URL}' < /etc/nginx/templates/default.conf.template \
+envsubst '${API_URL} ${PORT}' < /etc/nginx/templates/default.conf.template \
     > /etc/nginx/conf.d/default.conf
 
 exec "$@"
